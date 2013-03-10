9a04608f8aa5036f90e336879e3875daf0c1ccea
sfrench <sfrench@seismo.berkeley.edu>
2013-03-09 21:48:52 -0800

    New tests
    The test suite now checks for:
     1) Proper initialization of the foreign interpolant structure;
     2) The interpolation property (at one static abcissa) to within
        numerical precision.

--- tests/Test.hs
+++ tests/Test.hs
@@ -4,25 +4,51 @@ import Test.HUnit
 import Math.GSLInterp
 import qualified Data.Vector.Unboxed as V
 import Data.Maybe (isJust)
+import System.Exit
 
-testInit :: Maybe InterpStructPtr -> Test
-testInit s = TestCase (assertBool "interpInit" (isJust s))
+eps :: Double
+eps = 1e-15
+
+epsEqual 
+  :: Double 
+  -> Double 
+  -> Bool
+epsEqual x y = r < eps
+  where r = abs (x - y) / max x y
+
+testInit 
+  :: Maybe InterpStructPtr 
+  -> Test
+testInit s = let check = isJust s in TestCase (assertBool "initialization" check)
+
+testInterp 
+  :: Maybe InterpStructPtr
+  -> Double
+  -> Double
+  -> Test
+testInterp s x y = 
+  let check = 
+        case s of 
+          Just p  -> epsEqual y $ interpEval p x
+          Nothing -> False
+  in TestCase (assertBool "Test: Interpolation property" check)
 
 interpTestSuite :: IO ()
 interpTestSuite = do
   -- create the test data
   let x  = 2.5
       nx = 21
+      it = 10
       dx = 2.0 * pi / (nx - 1)
       xs = V.map (dx*) $ V.fromList [0..nx - 1]
       ys = V.map sin xs
   -- initialize the interpolant
   s <- interpInit xs ys
-  -- run the tests
-  runTestTT $ TestList [testInit s]
-  case s of 
-    Just p  -> interpFree p
-    Nothing -> return ()
+  -- run the tests and exit appropriately
+  counts <- runTestTT $ TestList [testInit s, testInterp s (xs V.! it) (ys V.! it)]
+  if failures counts > 0 
+    then exitFailure
+    else exitSuccess
 
 main :: IO ()
 main = interpTestSuite
