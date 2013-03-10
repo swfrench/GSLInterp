d09caa6365a84daa95ae8bba5f2fdad80b791e0a
sfrench <sfrench@seismo.berkeley.edu>
2013-03-09 20:57:08 -0800

    Basic testing (HUnit) now included

--- Test.hs
+++ Test.hs
@@ -1,23 +1,28 @@
-import GSLInterp
-import Text.Printf
-import qualified Data.Vector.Unboxed as V
+module Main where
 
-test :: IO ()
-test = do
+import Test.HUnit
+import Math.GSLInterp
+import qualified Data.Vector.Unboxed as V
+import Data.Maybe (isJust)
+
+testInit :: Maybe InterpStructPtr -> Test
+testInit s = TestCase (assertBool "interpInit" (isJust s))
+
+interpTestSuite :: IO ()
+interpTestSuite = do
+  -- create the test data
   let x  = 2.5
       nx = 21
       dx = 2.0 * pi / (nx - 1)
       xs = V.map (dx*) $ V.fromList [0..nx - 1]
       ys = V.map sin xs
+  -- initialize the interpolant
   s <- interpInit xs ys
-  case s of
-    Just p  -> sequence_
-      [ printf "%+e ~ %+e\n" (interpEval p x) (sin x)
-      , printf "%+e ~ %+e\n" (interpEvalDeriv p x) (cos x)
-      , printf "%+e ~ %+e\n" (interpEvalSecondDeriv p x) (- (sin x))
-      , printf "%+e ~ %+e\n" (interpEvalInteg p x (x + 0.1)) (- (cos (x + 0.1) - cos x))
-      , interpFree p]
+  -- run the tests
+  runTestTT $ TestList [testInit s]
+  case s of 
+    Just p  -> interpFree p
     Nothing -> return ()
 
 main :: IO ()
-main = test
+main = interpTestSuite
